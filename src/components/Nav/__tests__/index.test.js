86f77cadfefb2fe6e2b743a53de9d4b9aa0b6981
Kris Pennimpede <kpennimpede@hotmail.com>
2021-04-23 21:15:48 -0400

    finished gallery component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,17 +3,31 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
   //first test
   it('renders', () => {
-    render(<Nav />);
+    render(<Nav
+      categories={categories} 
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockSetCurrentCategory}
+      />);
   });
 
   //snapshot test
   it('matches snapshot', () => {
-    const { asFragment } = render(<Nav />);
+    const { asFragment } = render(<Nav
+      categories={categories} 
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockSetCurrentCategory}
+      />);
     //assert value comparison
     expect(asFragment()).toMatchSnapshot();
   });
@@ -22,7 +36,11 @@ describe('Nav component', () => {
 describe('emoji is visble', () => {
   it('inserts emoji into the h2', () => {
     //arrange
-    const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(<Nav
+      categories={categories} 
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockSetCurrentCategory}
+      />);
     //assert
     expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
@@ -31,9 +49,13 @@ describe('emoji is visble', () => {
 describe('links are visible', () => {
   it('inserts text into links', () => {
     //Arrange
-    const { getByTestId } = render(<Nav />);
+    const { getByTestId } = render(<Nav
+      categories={categories} 
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockSetCurrentCategory}
+      />);
     //Assert
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-    expect(getByTestId('about')).toHaveTextContent('About Me');
+    expect(getByTestId('about')).toHaveTextContent('About me');
   });
 })
